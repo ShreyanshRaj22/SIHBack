0f3373f55c3c73fda560083cb8e6e263acd662b8
Shreyansh Raj <211230051@nitdelhi.ac.in>
2024-04-07 13:23:28 +0530

    Containerizing the app

--- db.js
+++ db.js
@@ -6,12 +6,10 @@ const mongoDB = async () => {
     try {
         await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('Connected to MongoDB');
-        // to fetch data from mongoDB Atlas existing collection sample
+
         let fetched_data = await mongoose.connection.db.collection("sample")
         let caste_data = await mongoose.connection.db.collection("dataCaste")
         let gender_data = await mongoose.connection.db.collection("dataGender")
-        // using .find() and giving empty object we fetch all entries 
-        //and then convert them to an array using toArray()
         let districtData = await fetched_data.find({}).toArray();
         let casteData = await caste_data.find({}).toArray();
         let genderData = await gender_data.find({}).toArray();
